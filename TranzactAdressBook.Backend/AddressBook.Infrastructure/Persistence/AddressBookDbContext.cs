584d2c60b1d00f44d395b41032bcbca1250af3fb
Jose Aguirre <jaguirre@ktmc.com>
2023-07-06 09:18:12 -0500

    Add in memory configuration

--- TranzactAdressBook.Backend/AddressBook.Infrastructure/Persistence/AddressBookDbContext.cs
+++ TranzactAdressBook.Backend/AddressBook.Infrastructure/Persistence/AddressBookDbContext.cs
@@ -2,13 +2,20 @@
 using Microsoft.EntityFrameworkCore;
 using System.IO;
 
-namespace AddressBook.Infrastructure
+namespace AddressBook.Infrastructure.Persistence
 {
     public class AddressBookDbContext : DbContext
     {
         public AddressBookDbContext(DbContextOptions<AddressBookDbContext> options) : base(options)
         {
         }
+
+        // Configure InMemory DB
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "InMemoryAddressBook");
+        }
+
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Email> Emails { get; set; }
         public DbSet<Person> People { get; set; }
