76821d4c0c53db6f4b4b689db19b018d218ecd06
Jose Aguirre <jaguirre@ktmc.com>
2023-07-06 09:19:15 -0500

    Apply DI and repository into controllers

--- TranzactAdressBook.Backend/AddressBook.API/Program.cs
+++ TranzactAdressBook.Backend/AddressBook.API/Program.cs
@@ -1,19 +1,28 @@
-using AddressBook.Infrastructure;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
+using AddressBook.Application.Contracts.Persistence;
+using AddressBook.Infrastructure.Persistence;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Config DB
+// Config DBContext in memory
+builder.Services.AddDbContext<AddressBookDbContext>();
+// Config DBContext with SQL Server
+/*
 builder.Services.AddDbContext<AddressBookDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("local");
-    options.UseSqlServer(connectionString);
+    // options.UseSqlServer(connectionString);
 });
+*/
 
-builder.Services.AddControllers();
+// Add services to the Dependency Container.
+// builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddTransient<IPersonRepository, PersonRepository>();
+
+// Fix error:  A possible object cycle was detected. This can either be due to a cycle or if the object depth is larger than the maximum allowed depth of 32.
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
